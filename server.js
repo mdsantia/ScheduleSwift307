57b41fea1efcc804ca4ede1df1c21aff9e6f480d
James Corder <jamescorder@Jamess-MacBook-Pro.local>
2022-09-21 15:50:51 -0400

    Fixed Minor issue with placeholder port number

--- server.js
+++ server.js
@@ -6,34 +6,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Route to Homepage
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/static/login.html');
-  });
+  res.sendFile(__dirname + '/static/login.html');
+});
 
 
 // Route to Login Page
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/static/login.html');
-  });
+  res.sendFile(__dirname + '/static/login.html');
+});
 
 // Route to Main Page
 app.get('/home', (req, res) => {
-    res.sendFile(__dirname + '/static/index.html');
+  res.sendFile(__dirname + '/static/index.html');
 })
 
 
-  app.post('/login', (req, res) => {
-    // Insert Login Code Here
-    let username = req.body.username;
-    let password = req.body.password;
-    // Admin Login, Bypassess Verification
-    if (username == "sysadmin" && password == "sys1234") {
-        res.sendFile(__dirname + '/static/index.html');
-    } else {
-        res.send(`Username: ${username} Password: ${password}`);
-    }
-  });
+app.post('/login', (req, res) => {
+  // Insert Login Code Here
+  let username = req.body.username;
+  let password = req.body.password;
+  // Admin Login, Bypassess Verification
+  if (username == "sysadmin" && password == "sys1234") {
+    res.sendFile(__dirname + '/static/index.html');
+  } else {
+    res.send(`Username: ${username} Password: ${password}`);
+  }
+});
 
-  const port = 3001 // Port we will listen on
+const port = 3001 // Port we will listen on
 
-  // Function to listen on the port
-  app.listen(port, () => console.log(`This app is listening on port ${port}`));
+// Function to listen on the port
+app.listen(port, () => console.log(`This app is listening on port ${port}`));
