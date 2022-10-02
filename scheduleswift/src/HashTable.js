7c150e05fc65c54acef616ddec44330d19cf80e3
Micky <mdsantia@purdue.edu>
2022-10-02 03:36:02 -0400

    Made Changes to HashTable class included example

--- scheduleswift/src/HashTable.js
+++ scheduleswift/src/HashTable.js
@@ -9,8 +9,10 @@ class HashTable {
         this.object= {};
         this.size = 0;
         this.length = 0;
-  }
+    }
 
+
+    /** TODO: Redefine hashFunc to generate unique identifier */
     hashFunc(key) {
         return key.toString().length % this.size;
     }
@@ -23,15 +25,30 @@ class HashTable {
         if (!this.object[hash].hasOwnProperty(key)) {
             this.length++;
         }
-            this.object[hash][key] = value;
+        this.object[hash][key] = value;
     }
 
     searchFunction(key) {
         const hash = this.hashFunc(key);
         if (this.object.hasOwnProperty(hash) && this.object[hash].hasOwnProperty(key)) {
-                return this.object[hash][key];
-            } else {
-                return null;
-            }
+            return this.object[hash][key];
+        } else {
+            return null;
         }
+    }
+
 }
+
+/** Example of how to initialize a hashtable to contain different values */
+
+/**
+const hashtable = new HashTable();
+hashtable.addPair("Alex", "01");
+hashtable.addPair("Stepheny", "23");
+hashtable.addPair("Max", "90");
+console.log(hashtable.searchFunction("Max"));
+*/
+
+/** To run, just install Code Runner Extension in VSCode and Control+ALT N or Control+Option N 
+ * Or F1 and then select Run Code and the output should appear
+*/
