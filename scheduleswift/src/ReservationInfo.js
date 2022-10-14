31803b6c27ddbc37eacccf465e5fb1127d37e2f2
mdsantia1720 <82775895+mdsantia1720@users.noreply.github.com>
2022-10-14 03:05:11 -0400

    Add files via upload

--- scheduleswift/src/ReservationInfo.js
+++ scheduleswift/src/ReservationInfo.js
@@ -4,8 +4,9 @@ import './ReservationForm.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ReservationInfo from "./Components/ReservationInfo.component";
-import Form from "./Components/makeReservation.component";
+import ReservationForm from "./Components/makeReservation.component";
 import Search from "./Components/searchEvent.component";
+import MainPage from "./Components/mainPage.component";
 
 function Reservationinfo() {
     return (
@@ -13,7 +14,8 @@ function Reservationinfo() {
             <div className="Reservation Info">
                     <Routes>
                             <Route exact path="/" element={<Search />} />
-                            <Route exact path="/form" element={<Search />} />
+                            <Route exact path="/main" element={<MainPage />} />
+                            <Route exact path="/form" element={<ReservationForm />} />
                             <Route exact path="/info" element={<ReservationInfo />} />
                     </Routes>
             </div>
