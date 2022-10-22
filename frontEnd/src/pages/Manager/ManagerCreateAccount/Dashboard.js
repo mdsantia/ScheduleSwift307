4980be1b820caca031122b1a8b3ee889baa08268
James Corder <jamescorder@Jamess-MacBook-Pro.local>
2022-10-22 17:04:41 -0400

    Finished User Story #1: Manager can create acct for Employees

--- frontEnd/src/pages/Manager/ManagerCreateAccount/Dashboard.js
+++ frontEnd/src/pages/Manager/ManagerCreateAccount/Dashboard.js
@@ -245,7 +245,7 @@ function DashboardContent() {
                             {/* Recent Orders */}
                             <Grid item xs={12}>
                                 <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                                    <Orders />
+                                    <Orders username={state.username} password={state.password} businessName={state.businessName} />
                                 </Paper>
                             </Grid>
                         </Grid>
