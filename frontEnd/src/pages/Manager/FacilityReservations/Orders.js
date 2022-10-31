e125f1a32e1e9e89bfc32d2fe0d70893944e697a
Domthebackup <83187380+Domthebackup@users.noreply.github.com>
2022-10-31 19:19:39 -0400

    Added Change Reservation Price for managers

--- frontEnd/src/pages/Manager/FacilityReservations/Orders.js
+++ frontEnd/src/pages/Manager/FacilityReservations/Orders.js
@@ -5,10 +5,12 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Box from '@mui/material/Box';
 import Title from './Title';
 import Axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Button, TextField, Grid, Typography } from '@mui/material';
+import { Button, TextField, Grid, Typography, TableFooter } from '@mui/material';
+import { InputLabel } from '@mui/material';
 
 function preventDefault(event) {
     event.preventDefault();
@@ -38,6 +40,26 @@ export default function Orders(props) {
                 setError("");
                 getReservations(props.businessName);
             }
+        })
+    }
+
+    const changePrice = (event) => {
+
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+
+        console.log(data.get('resID'));
+        console.log(data.get('newPrice'));
+
+        Axios.post("http://localhost:3001/api/managerChangePrice", {    
+
+            id: data.get('resID'),
+            newPrice: data.get('newPrice'),
+
+        }).then(() => {
+
+            window.location.reload(false);
+
         })
     }
 
@@ -73,6 +95,36 @@ export default function Orders(props) {
                         ))}
                     </TableBody>
                 </Table>
+                <br></br>
+                <h3>Change Reservation Prices</h3>
+                <Box component="form" validate="true" onSubmit={changePrice} sx={{ mt: 3 }}>
+                    <Grid container spacing={0}>
+                        <Grid xs={7}>
+                            <TextField
+                                placeholder="Reservation ID"
+                                name="resID"
+                                id="resID"
+                                required
+                            />
+                        </Grid>
+                        <Grid>
+                        <TextField
+                                placeholder="New Price"
+                                id="newPrice"
+                                name="newPrice"
+                                required
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        fullWidth
+                        type="submit"
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Change
+                    </Button>
+                </Box>
             </React.Fragment >
         );
     } else {
