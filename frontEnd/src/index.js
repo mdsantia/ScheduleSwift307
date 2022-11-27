470884b853edc11ce5dd3d9cb044cd0172fe8217
mdsantia1720 <82775895+mdsantia1720@users.noreply.github.com>
2022-11-26 19:21:28 -0500

    Localhost is the default

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -5,8 +5,9 @@ import App from './App';
 
 export function getIP() {
   // Set the ip to be the host of the database
-  // const ip = 'localhost';
-  const ip = '192.168.1.15';
+  const ip = 'localhost';
+  // enter local network ip to work given by the terminal in the on local network field
+  // const ip = '192.168.1.15';
   return ip;
 }
 
